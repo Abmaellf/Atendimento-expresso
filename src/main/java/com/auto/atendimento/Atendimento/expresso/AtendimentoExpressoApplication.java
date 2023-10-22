a9a951cfaaad10e5a9fa8aba41b281521b4d2f98
Abmaellf <abmael.ninha@gmail.com>
2023-10-22 17:32:08 -0400

    first commit

---
+++ src/main/java/com/auto/atendimento/Atendimento/expresso/AtendimentoExpressoApplication.java
@@ -0,0 +1,13 @@
+package com.auto.atendimento.Atendimento.expresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentoExpressoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentoExpressoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/atendimento/Atendimento/expresso/AtendimentoExpressoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.atendimento.Atendimento.expresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentoExpressoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
